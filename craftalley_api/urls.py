6a5764531dd41016712bcf77f14d4ef3fa6b01af
konstantin <kkononenko3@gmail.com>
2023-10-09 15:39:39 +0300

    created API + added drf spectacular + added django debug toolbar + added dotenv

---
+++ craftalley_api/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("user/", include("user.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
