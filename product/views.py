1eeb5c9386bd58e020d8452c498147e9a154068b
konstantin <kkononenko3@gmail.com>
2023-10-10 13:17:46 +0300

    created view set for catalog and category

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from product.models import Catalog, Category
+from product.serializers import CatalogSerializer, CategorySerializer
+
+
+class CatalogViewSet(viewsets.ModelViewSet):
+    queryset = Catalog.objects.all()
+    serializer_class = CatalogSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
