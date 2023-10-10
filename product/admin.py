ddecd79ebc3b3bf0ee3bd6e22b4d107c3b913f9a
konstantin <kkononenko3@gmail.com>
2023-10-10 11:02:31 +0300

    created admin functionality for catalog, category, product, rating

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Catalog, Category, Product, Rating, RatingStarProduct
 
-# Register your models here.
+
+@admin.register(Catalog)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "manufacturer",
+        "price",
+        "created",
+        "updated",
+    ]
+    list_filter = ["created", "updated"]
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("id", "star", "product")
+
+
+admin.site.register(RatingStarProduct)
