5378bbc68a31a6439eb6010561a165318475f855
konstantin <kkononenko3@gmail.com>
2023-10-10 15:22:16 +0300

    added permissions for product/catalog/rating/category

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from django.db import models
-
+from rest_framework.permissions import IsAuthenticated
 
 from product.check_ip_for_rating import get_client_ip
 from product.models import Catalog, Category, Product, Rating
+from product.permissions import IsAdminOrIfAuthenticatedReadOnly
 from product.serializers import CatalogSerializer, CategorySerializer, CategoryListSerializer, CategoryDetailSerializer, \
     ProductSerializer, ProductListSerializer, ProductDetailSerializer, CreateRatingSerializer
 
@@ -19,6 +20,7 @@ class CatalogViewSet(viewsets.ModelViewSet):
     queryset = Catalog.objects.all()
     serializer_class = CatalogSerializer
     pagination_class = CatalogPagination
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class CategoryPagination(PageNumberPagination):
@@ -30,6 +32,7 @@ class CategoryPagination(PageNumberPagination):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     pagination_class = CategoryPagination
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -48,6 +51,7 @@ class ProductPagination(PageNumberPagination):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     pagination_class = ProductPagination
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         product = (
@@ -76,6 +80,8 @@ class ProductViewSet(viewsets.ModelViewSet):
 class AddStarRatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = CreateRatingSerializer
+    permission_classes = (IsAuthenticated,)
+
 
     def perform_create(self, serializer):
         serializer.save(ip=get_client_ip(self.request))
