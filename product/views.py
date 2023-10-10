fbbd871c33db46afeab39cbab49feffe9a45ba28
konstantin <kkononenko3@gmail.com>
2023-10-10 14:19:09 +0300

    added pagination for catalog, category and product

--- product/views.py
+++ product/views.py
@@ -1,17 +1,32 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from product.models import Catalog, Category, Product
 from product.serializers import CatalogSerializer, CategorySerializer, CategoryListSerializer, CategoryDetailSerializer, \
     ProductSerializer, ProductListSerializer, ProductDetailSerializer
 
 
+class CatalogPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class CatalogViewSet(viewsets.ModelViewSet):
     queryset = Catalog.objects.all()
     serializer_class = CatalogSerializer
+    pagination_class = CatalogPagination
+
+
+class CategoryPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 100
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
+    pagination_class = CategoryPagination
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -21,8 +36,15 @@ class CategoryViewSet(viewsets.ModelViewSet):
         return CategorySerializer
 
 
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
+    pagination_class = ProductPagination
 
     def get_serializer_class(self):
         if self.action == "list":
