1e20d02742acfa19570007d16035ff7f68298d4e
konstantin <kkononenko3@gmail.com>
2023-10-10 17:33:38 +0300

    added some new functionality for comment serializers

--- comment/serializers.py
+++ comment/serializers.py
@@ -9,8 +9,17 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "content",
-            "image",
-            "user",
+            "date_posted",
+            "product",
+        )
+
+
+class CommentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            "id",
+            "content",
             "date_posted",
             "product",
         )
@@ -23,10 +32,16 @@ class CommentDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = (
-            "id",
             "content",
-            "image",
-            "user",
             "date_posted",
-            "product",
+            "first_name",
+            "last_name"
         )
+
+
+class CommentImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(read_only=False)
+
+    class Meta:
+        model = Comment
+        fields = ("id", "image")
