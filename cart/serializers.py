37b97a701f0747c663f7446955a2dc3520d480b4
konstantin <kkononenko3@gmail.com>
2023-10-12 15:37:34 +0300

    deleted user from serializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
-        fields = ["id", "user", "item", "quantity", "created_at", "updated_at"]
+        fields = ["id", "item", "quantity", "created_at", "updated_at"]
 
 
 class DeliveryCostSerializer(serializers.ModelSerializer):
